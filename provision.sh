a8fff0eae17d3127cd7ce61321fc4a2d4556839e
Nikita Maruniak <nikita.marunyak@gmail.com>
2023-05-04 01:11:15 +0300

    Upgrade the system during its provisioning

--- provision.sh
+++ provision.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
+sudo apt-get -y dist-upgrade
 sudo apt-get -y install postgresql-15 postgresql-client-15 libpq-dev
 
 # Create the DB and its user
